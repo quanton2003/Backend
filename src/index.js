bcbe923fb559e884898b7495ce97371bd9f56443
quanton2003 <125117562+quanton2003@users.noreply.github.com>
2025-03-05 02:38:08 +0700

    Thêm file mới

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config()
+const app = express();
+const port = process.env.PORT || 3001;
+app.get('/', (req, res) =>{
+    return res.send('hello bonj nay')
+})
+app.listen(port,() =>{
+    console.log(`Server is running on port ${port}`);
+} )
