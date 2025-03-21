a1767ca9341e38836add6b0b902bee8dba549d97
quanton2003 <125117562+quanton2003@users.noreply.github.com>
2025-03-21 11:37:22 +0700

    update

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { authMiddleWare,authUserMiddleWare } = require("../middleware/authMiddleware")
 const ProductController = require('../controllers/ProductController')
 router.post('/create', ProductController.createProduct);
-router.put('/update-product/:id', authMiddleWare, ProductController.updateProduct);  
+router.put('/update-product/:id', authUserMiddleWare, ProductController.updateProduct);  
 router.get('/details-product/:id', ProductController.getDetailsProduct); 
 router.delete('/delete-product/:id', ProductController.deleteProduct);  
 router.get('/get-all', ProductController.getAllProduct);  
